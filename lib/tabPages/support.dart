4f04c672c6cdf7a4ef901258a1fb708f17c1c92c
TalhaShakel <shakeeltalha637@gmail.com>
2022-11-08 12:32:53 +0500

    Started

--- lib/tabPages/support.dart
+++ lib/tabPages/support.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class support extends StatelessWidget {
   const support({super.key});
@@ -6,7 +8,85 @@ class support extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        // backgroundColor: mobileBackgroundColor,
+        title: const Text(
+          'Support',
+        ),
+        centerTitle: false,
+      ),
+      body: Column(children: [
+        
+      ],),
     );
+  }
+}
+
+class CommentCard extends StatelessWidget {
+  final snap;
+  const CommentCard({Key? key, required this.snap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return snap == null
+        ? CircularProgressIndicator()
+        : Container(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  backgroundImage:
+                      NetworkImage(snap.data()["photoUrl"].toString()),
+                  radius: 18,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // RichText(
+                        //   text: TextSpan(
+                        //     children: [
+                        //       TextSpan(
+                        //           text: snap.data()['name'],
+                        //           style: const TextStyle(
+                        //             fontWeight: FontWeight.bold,
+                        //           )),
+                        //       TextSpan(
+                        //         text: ' ${snap}',
+                        //       ),
+                        //     ],
+                        //   ),
+                        // ),
+                        Text(snap.data()['name']),
+                        Text(snap.data()['comment']),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: Text(
+                            DateFormat.yMMMd().format(
+                              snap.data()['datePublished'].toDate(),
+                            ),
+                            style: const TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Icon(
+                    Icons.favorite,
+                    size: 16,
+                  ),
+                )
+              ],
+            ),
+          );
   }
 }
