a6b706192701183d6e40deb3bdfdbfb41f44603d
TalhaShakel <shakeeltalha637@gmail.com>
2022-11-10 16:14:17 +0500

    Started

--- lib/tabPages/profile_tab.dart
+++ lib/tabPages/profile_tab.dart
@@ -1,3 +1,4 @@
+import 'package:drivers_app/Support/support.dart';
 import 'package:drivers_app/authentication/login_screen.dart';
 import 'package:drivers_app/controller.dart';
 import 'package:drivers_app/global/global.dart';
@@ -25,103 +26,110 @@ class _ProfileTabPageState extends State<ProfileTabPage> {
     return Scaffold(
       backgroundColor: theme.scaffoldBackgroundColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //name
-            Text(
-              onlineDriverData.name!,
-              style: TextStyle(
-                color: theme.primaryColor,
-                fontSize: 50.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            Text(
-              titleStarsRating + "driver",
-              style: TextStyle(
-                color: theme.primaryColor,
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(
-              height: 20,
-              width: 200,
-              child: Divider(
-                color: Colors.white,
-                height: 2,
-                thickness: 2,
-              ),
-            ),
-
-            const SizedBox(
-              height: 38.0,
-            ),
-
-            //phone
-            InfoDesignUIWidget(
-              textInfo: onlineDriverData.phone!,
-              iconData: Icons.phone_iphone,
-            ),
-
-            //email
-            InfoDesignUIWidget(
-              textInfo: onlineDriverData.email!,
-              iconData: Icons.email,
-            ),
-
-            InfoDesignUIWidget(
-              textInfo: onlineDriverData.car_color! +
-                  "" +
-                  onlineDriverData.car_model! +
-                  "" +
-                  onlineDriverData.car_number!,
-              iconData: Icons.car_repair,
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            ElevatedButton(
-                onPressed: () {
-                  fAuth.signOut();
-                  Get.to(() => LoginScreen());
-                  // SystemNavigator.pop();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.redAccent,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //name
+              Text(
+                onlineDriverData.name!,
+                style: TextStyle(
+                  color: theme.primaryColor,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                child: Text(
-                  "Logout",
-                  style: TextStyle(
-                    color: theme.primaryColor,
+              ),
+
+              Text(
+                titleStarsRating + "driver",
+                style: TextStyle(
+                  color: theme.primaryColor,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              const SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.white,
+                  height: 2,
+                  thickness: 2,
+                ),
+              ),
+
+              const SizedBox(
+                height: 38.0,
+              ),
+
+              //phone
+              InfoDesignUIWidget(
+                textInfo: onlineDriverData.phone!,
+                iconData: Icons.phone_iphone,
+              ),
+
+              //email
+              InfoDesignUIWidget(
+                textInfo: onlineDriverData.email!,
+                iconData: Icons.email,
+              ),
+
+              InfoDesignUIWidget(
+                textInfo: onlineDriverData.car_color! +
+                    "" +
+                    onlineDriverData.car_model! +
+                    "" +
+                    onlineDriverData.car_number!,
+                iconData: Icons.car_repair,
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              ElevatedButton(
+                  onPressed: () {
+                    fAuth.signOut();
+                    Get.to(() => LoginScreen());
+                    // SystemNavigator.pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.redAccent,
                   ),
-                )),
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(
+                      color: theme.primaryColor,
+                    ),
+                  )),
 
-            Obx(() => IconButton(
+              Obx(() => IconButton(
 
-                // tooltip: "asd",
-                onPressed: () {
-                  controller.changeTheme();
-                },
-                icon: Icon(
-                  controller.isDark.value ? Icons.light_mode : Icons.dark_mode,
-                  color: theme.iconTheme.color,
-                ))),
-            Text(
-              controller.isDark.value ? " Dark mode" : "Light mode",
-              style: TextStyle(color: theme.primaryColor),
-            ),
-            TextButton(
-                onPressed: () {
-                  Get.to(() => support());
-                },
-                child: Text("Contact Support team"))
-          ],
+                  // tooltip: "asd",
+                  onPressed: () {
+                    controller.changeTheme();
+                  },
+                  icon: Icon(
+                    controller.isDark.value
+                        ? Icons.light_mode
+                        : Icons.dark_mode,
+                    color: theme.iconTheme.color,
+                  ))),
+              Text(
+                controller.isDark.value ? " Dark mode" : "Light mode",
+                style: TextStyle(color: theme.primaryColor),
+              ),
+
+              TextButton(
+                  onPressed: () {
+                    Get.to(() => Support_page(
+                          email: onlineDriverData.email.toString(),
+                        ));
+                  },
+                  child: Text("Contact Support team"))
+            ],
+          ),
         ),
       ),
     );
